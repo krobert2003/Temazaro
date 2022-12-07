392f1c7a7545c87fe62e7220be7ef354e14ebda1
user <user@B101-02>
2022-12-07 16:09:50 +0100

    Add project files.

---
+++ Temazaro/Torol.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Temazaro
+{
+    public partial class Torol : Form
+    {
+        public Torol()
+        {
+            InitializeComponent();
+        }
+
+        private void Torol_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            Program.cmd.CommandText = "DELETE FROM `konyv` WHERE Id = @id";
+            Program.cmd.Parameters.Clear();
+            Program.cmd.Parameters.AddWithValue("@id", textBox1.Text);
+            Program.cmd.Parameters.AddWithValue("@szezo", textBox2.Text);
+            Program.cmd.Parameters.AddWithValue("@fajta", textBox4.Text);
+            Program.cmd.Parameters.AddWithValue("@cim", textBox3.Text);
+
+            Program.conn.Open();
+            if (Program.cmd.ExecuteNonQuery() == 1)
+            {
+                MessageBox.Show("Sikeresen törlés!");
+                Program.conn.Close();
+                textBox1.Text = "";
+                textBox2.Text = "";
+                textBox3.Text = "";
+                textBox4.Text = "";
+
+              
+            }
+            else
+            {
+                MessageBox.Show("sikertelen  törlés!");
+            }
+
+        }
+    }
+}
